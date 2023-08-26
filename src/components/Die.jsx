241964ef3fd4e9d79b18a763cab2576b1bc82441
Dzabol <sebastian.jablecki@gmail.com>
2023-08-26 20:14:47 +0200

    photos rename

--- src/components/Die.jsx
+++ src/components/Die.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Die(props) {
   const style = `die ${props.properties.isActive ? "die-active" : ""}`;
   const dieValue = props.properties.value;
-  const dieName = `Dice-${dieValue}`;
+  const dieName = `Dice${dieValue}`;
   const imageSrc = `../images/${dieName}.PNG`;
 
   return (
