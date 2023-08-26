8787e29db01abfcbae8e8c831c273886ba8b1e13
Dzabol <sebastian.jablecki@gmail.com>
2023-08-26 19:43:15 +0200

    corrected link

--- src/components/Die.jsx
+++ src/components/Die.jsx
@@ -4,7 +4,7 @@ export default function Die(props) {
   const style = `die ${props.properties.isActive ? "die-active" : ""}`;
   const dieValue = props.properties.value;
   const dieName = `Dice-${dieValue}`;
-  const imageSrc = `./src/images/${dieName}.png`;
+  const imageSrc = `../src/images/${dieName}.png`;
 
   return (
     <div className={style} onClick={props.clickHandler}>
