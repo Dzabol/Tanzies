2f64352edc80646e3cc2d7325c5515509c5b98d3
Dzabol <sebastian.jablecki@gmail.com>
2023-08-26 20:24:56 +0200

    Update Die.jsx

--- src/components/Die.jsx
+++ src/components/Die.jsx
@@ -3,8 +3,9 @@ import React from "react";
 export default function Die(props) {
   const style = `die ${props.properties.isActive ? "die-active" : ""}`;
   const dieValue = props.properties.value;
-  const dieName = `Dice${dieValue}`;
+  const dieName = `dice${dieValue}`;
   const imageSrc = `../images/${dieName}.PNG`;
+  console.log(imageSrc);
 
   return (
     <div className={style} onClick={props.clickHandler}>
